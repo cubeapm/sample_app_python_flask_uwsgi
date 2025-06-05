f164d3f9075ffa7582dc7a523b2d47d4ea171a15
ishaan766 <ishaan766@gmail.com>
2025-06-05 16:18:33 +0530

    added TracerProvider with SERVICE_NAME and HOST_NAME resource

--- tracing.py
+++ tracing.py
@@ -1,16 +1,21 @@
 import os
 from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.semconv.resource import ResourceAttributes
+from opentelemetry.sdk.trace import TracerProvider, Resource
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
     ConsoleSpanExporter,
     SimpleSpanProcessor,
 )
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from socket import gethostname
 
 
 def init_tracing():
-    provider = TracerProvider()
+    provider = TracerProvider(resource=Resource({
+      ResourceAttributes.SERVICE_NAME: os.environ['OTEL_SERVICE_NAME'],
+      ResourceAttributes.HOST_NAME: gethostname() or 'UNSET',
+   }))
     if os.getenv('OTEL_LOG_LEVEL', '') == 'debug':
         processor = SimpleSpanProcessor(ConsoleSpanExporter())
     else:
